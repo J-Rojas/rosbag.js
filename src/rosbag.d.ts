f1effb8043aa8931a805ddbce2847582c41c3538
Jose Rojas <jrojas@redlinesolutions.co>
2021-08-25 05:24:12 -0700

    Add typescript definitions

---
+++ src/rosbag.d.ts
@@ -0,0 +1,36 @@
+declare module 'rosbag' {
+    import { IndexableObject } from "ros-web-vue/src/config/types";
+    import { CachedMessage, Timestamp } from "ros-web-vue/src/core/types";
+
+    export interface BagConnectionEntry {
+        type: string
+        topic: string
+    }
+
+    export default class Bag {
+        constructor(reader: BagReader)
+        open(): Promise<void>
+
+        startTime: Timestamp
+        endTime: Timestamp
+
+        readMessages(params: {
+            startTime: Timestamp,
+            endTime: Timestamp,
+            decompress: IndexableObject<Function>,
+            pendingMap: unknown
+        }, cb: (msg: CachedMessage) => void): Promise<void>
+
+        readonly connections: IndexableObject<BagConnectionEntry>
+    }
+
+    export class BagReader {
+        constructor(loader: any)
+    }
+
+    export interface BagConnectionEntry {
+
+    }
+
+    export function open(url: string | Blob): Promise<Bag>
+}
